e110c36d2438f98c5458af7fcab5c22ed3b9f92e
abhi281500 <abhi281500@gmail.com>
2026-03-23 00:55:14 +0530

    fix baseURl

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-    baseURL: "https://authforge-7por.onrender.com"
+    baseURL: "https://authforge-7por.onrender.com/api"
 })
 
 export default API
